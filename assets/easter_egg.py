c99d98c954297e5ffbba6617ca4edb6b3390642e
Jone73 <jone@stainedblood.com>
2026-07-18 13:47:07 +0300

    Create easter_egg.py

---
+++ assets/easter_egg.py
@@ -0,0 +1,21 @@
+SUCCESS = [
+    "Rolling on the river...",
+    "From pixels to sawdust.",
+    "All systems in tune.",
+    "Ready to make chips.",
+    "The router approves.",
+    "Wood is waiting..."
+]
+
+VALIDATOR = [
+    "Measure twice... You only measured once.",
+    "The wood disagrees.",
+    "This neck has stage fright.",
+]
+
+OWL = r"""
+      ,_,
+     (O,O)
+     (   )
+      " "
+"""
